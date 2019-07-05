420f454575687c0ab29f9c5d9b787d04971303cc
Kevin Kee <kevinkee@Kevins-MacBook-Pro.local>
2019-07-05 14:39:20 +0900

    add jagged border to header

--- functions.php
+++ functions.php
@@ -6,11 +6,11 @@ require_once( 'inc/class-woocommerce.php' );
 require_once( 'inc/class-yoast.php' );
 // require_once( 'inc/class-contact.php' );
 
-add_action( 'wp_enqueue_scripts', 'extract_storefront_inline', 150 );
-
-function extract_storefront_inline() {
-  $inline_style = WP_Styles()->registered['storefront-style']->extra['after'][0];
-  wp_register_style( 'customizer-style', false );
-  wp_enqueue_style( 'customizer-style' );
-  wp_add_inline_style( 'customizer-style', $inline_style );
-}
+// add_action( 'wp_enqueue_scripts', 'extract_storefront_inline', 150 );
+//
+// function extract_storefront_inline() {
+//   $inline_style = WP_Styles()->registered['storefront-style']->extra['after'][0];
+//   wp_register_style( 'customizer-style', false );
+//   wp_enqueue_style( 'customizer-style' );
+//   wp_add_inline_style( 'customizer-style', $inline_style );
+// }
--- inc/class-header.php
+++ inc/class-header.php
@@ -28,7 +28,22 @@ class Foodie_Japan_Header {
   /**
    * Header adjustments
    * @return rearrange header
+
+   * Functions hooked into storefront_header action
+   *
+   * @hooked storefront_header_container                 - 0
+   * @hooked storefront_skip_links                       - 5
+   * @hooked storefront_social_icons                     - 10
+   * @hooked storefront_site_branding                    - 20
+   * @hooked storefront_secondary_navigation             - 30
+   * @hooked storefront_product_search                   - 40
+   * @hooked storefront_header_container_close           - 41
+   * @hooked storefront_primary_navigation_wrapper       - 42
+   * @hooked storefront_primary_navigation               - 50
+   * @hooked storefront_header_cart                      - 60
+   * @hooked storefront_primary_navigation_wrapper_close - 68
    */
+
   public function header_adjustments() {
     function fj_preheader_text() {
       if ( is_active_sidebar( 'preheader_text' ) ) : ?>
@@ -46,15 +61,27 @@ class Foodie_Japan_Header {
       echo '</div>';
     }
 
+  	function fj_primary_navigation_wrapper() {
+  		echo '<div class="storefront-primary-navigation">';
+  	}
+
+    function fj_primary_navigation_wrapper_close() {
+      echo '</div>';
+    }
+
     remove_action( 'storefront_header', 'storefront_header_container', 0);
     remove_action( 'storefront_header', 'storefront_site_branding', 20);
     remove_action( 'storefront_header', 'storefront_product_search', 40);
+    remove_action( 'storefront_header', 'storefront_primary_navigation_wrapper', 42);
     remove_action( 'storefront_header', 'storefront_header_container_close', 41 );
+    remove_action( 'storefront_header', 'storefront_primary_navigation_wrapper_close', 68);
 
     add_action( 'storefront_header', 'fj_preheader', 0);
     add_action( 'storefront_header', 'fj_preheader_text', 25);
     add_action( 'storefront_header', 'fj_preheader_close', 40);
+    add_action( 'storefront_header', 'fj_primary_navigation_wrapper', 42);
     add_action( 'storefront_header', 'storefront_site_branding', 45);
+    add_action( 'storefront_header', 'fj_primary_navigation_wrapper_close', 68);
   }
 
   /**
