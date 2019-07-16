fef8b2a9e6a945beccb954e357cc2c909d7e38c4
Kevin Kee <kevinkee@Kevins-MacBook-Pro.local>
2019-07-16 11:19:00 +0900

    single product: template half done, need summary bar

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,8 @@
 (function($) {
   $(document).ready(function(e) {
     $.when( $('.product-slider').bxSlider({
-      auto: true,
       captions: true,
       keyboardEnabled: true,
-      infiniteLoop: false,
-      hideControlOnEnd: true,
       nextText: '<i class="fa fa-2x fa-chevron-right"></i>',
       prevText: '<i class="fa fa-2x fa-chevron-left"></i>'
     }) ).then(function() {
--- inc/class-woocommerce.php
+++ inc/class-woocommerce.php
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 if ( ! class_exists( 'Foodie_Japan_Woocommerce' ) ) :
 
-class Foodie_Japan_Woocommerce {
+class Foodie_Japan_Woocommerce extends WC_Bookings_Data {
   /**
    * Setup class.
    *
@@ -22,15 +22,43 @@ class Foodie_Japan_Woocommerce {
   public function __construct() {
     remove_action( 'woocommerce_before_single_product_summary', 'woocommerce_show_product_images', 20 );
     remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
-    // remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+    remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
+    remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
+
     add_filter( 'woocommerce_checkout_fields', array( $this, 'override_checkout_fields' ) );
+    add_filter( 'booking_form_fields', array( $this, 'custom_order_booking_fields') );
+
     add_action( 'wp_footer', array( $this, 'hide_empty_cart' ) );
-    add_action( 'woocommerce_before_single_product_summary', array( $this, 'product_images_slider'), 20 );
+    add_action( 'woocommerce_before_single_product_summary', array( $this, 'create_tour_summary'), 10 );
+    add_action( 'woocommerce_before_single_product_summary', array( $this, 'create_product_main_content'), 20 );
+    add_action( 'product_main_content', array( $this, 'product_images_slider'), 10 );
+    add_action( 'product_main_content', 'woocommerce_template_single_excerpt' );
+  }
+
+  /**
+   * Reorder booking fields
+   * @param  object $fields
+   * @return object
+   */
+  public function custom_order_booking_fields ( $fields ) {
+    $product = wc_get_product();
+    $persons = $product->get_person_types();
+
+  	$reorder  = array();
+    if ($fields['wc_bookings_field_start_date'])
+      $reorder[] = $fields['wc_bookings_field_start_date'];
+    if ($persons) : foreach (array_keys($persons) as $person) :
+      $reorder[] = $fields['wc_bookings_field_persons_' . $person];
+    endforeach; endif;
+    if ($fields['wc_bookings_field_resource'])
+      $reorder[] = $fields['wc_bookings_field_resource'];
+  	return $reorder;
   }
 
   /**
    * Unset billing
-   * @return void
+   * @param  object $fields
+   * @return object
    */
   public function override_checkout_fields( $fields ) {
    unset($fields['billing']['billing_company']);
@@ -45,7 +73,7 @@ class Foodie_Japan_Woocommerce {
 
   /**
    * Hide empty cart
-   * @return void
+   * @return string
    */
   public function hide_empty_cart() {
     if ( WC()->cart->get_cart_contents_count() == 0 ) {
@@ -56,20 +84,57 @@ class Foodie_Japan_Woocommerce {
   }
 
   /**
-   * Image single product
-   * @return void
+   * Product main content
+   * @return string
+   */
+  public function create_tour_summary() {
+    ?>
+    <div class="tour-summary">
+      qwer
+    </div>
+    <?php
+  }
+
+  /**
+   * Product main content
+   * @return string
+   */
+  public function create_product_main_content() {
+    global $product;
+    $gallery_images = $product->get_gallery_image_ids();
+
+    ?>
+    <div class="product-main-content">
+      <?php do_action('product_main_content'); ?>
+    </div>
+    <?php
+  }
+
+  /**
+   * Product media slider
+   * @return string
    */
   public function product_images_slider() {
     global $product;
     $gallery_images = $product->get_gallery_image_ids();
 
-    echo '<div class="product-media"><ul class="product-slider hidden">';
-      foreach ($gallery_images as $attachment_id) {
-        echo'<li><div class="product-photo">';
-          echo wp_get_attachment_image($attachment_id, 'photo-slider', false, array('title' => get_the_title($attachment_id)));
-        echo '</div></li>';
-      }
-    echo '</ul></div>';
+    ?>
+    <div class="product-slider-wrapper">
+      <ul class="product-slider hidden">
+        <?php foreach ($gallery_images as $index => $attachment_id) {
+          $terminal = '';
+          $terminal = $index === 0 ? 'photo-first' : $terminal;
+          $terminal = $index === count($gallery_images) - 1 ? 'photo-last' : $terminal;
+
+          ?>
+          <li class="<?php echo $terminal; ?>"><div class="product-photo">
+            <?php echo wp_get_attachment_image($attachment_id, 'photo-slider', false, array('title' => get_the_title($attachment_id))); ?>
+          </div></li>
+          <?php
+        } ?>
+      </ul>
+    </div>
+    <?php
   }
 }
 
