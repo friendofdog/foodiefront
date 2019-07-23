acfac602dd878cd747301e82af297644b4b44917
Kevin Kee <kevinkee@Kevins-MacBook-Pro.local>
2019-07-23 16:01:11 +0900

    fix polaroid grid on mobile

--- content-post-loop.php
+++ content-post-loop.php
@@ -1,6 +1,8 @@
-<a href="<?php echo get_post_permalink(); ?>" class="post-panel">
-  <?php echo get_the_post_thumbnail( $post, 'woocommerce_thumbnail' ); ?>
-  <div class="text-wrapper">
-    <h4 class="text-title"><?php the_title(); ?></h4>
-  </div>
-</a>
+<div class="post-panel-wrapper">
+  <a href="<?php echo get_post_permalink(); ?>" class="post-panel">
+    <?php echo get_the_post_thumbnail( $post, 'woocommerce_thumbnail' ); ?>
+    <div class="text-wrapper">
+      <h4 class="text-title"><?php the_title(); ?></h4>
+    </div>
+  </a>
+</div>
--- loop-posts.php
+++ loop-posts.php
@@ -2,20 +2,10 @@
 
 do_action( 'storefront_loop_before' );
 
-$count = 0;
-$index = 0;
 if( have_posts() ) :
-  echo '<div class="posts-grid posts-row-' . $index . '">';
+  echo '<div class="posts-grid">';
     while ( have_posts() ) : the_post();
-    if ($count < 3) {
       get_template_part( 'content-post-loop', get_post_format() );
-    } else {
-      $index = $index + 1;
-      echo '</div><div class="posts-grid posts-row-' . $index . '">';
-    	get_template_part( 'content-post-loop', get_post_format() );
-      $count = 0;
-    }
-    $count = $count + 1;
     endwhile; ?>
   </div>
 <?php endif;
